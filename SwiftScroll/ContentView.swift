d363202ac08e4ead14067aedbd5f778e568a73e7
Connor Lagana <connor.lagana@gmail.com>
2020-04-20 16:57:08 -0400

    okay

--- SwiftScroll/ContentView.swift
+++ SwiftScroll/ContentView.swift
@@ -8,14 +8,48 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct  Post: Identifiable {
+    var id: Int
+    let setsNum, repsNum, time: Int
+    let title: String
+}
+
+struct ContentViews: View {
+    
+    let posts: [Post] = [
+    .init(id: 0, setsNum: 8, repsNum: 25, time: 12, title: "Pushups"),
+    .init(id: 1, setsNum: 5, repsNum: 10, time: 30, title: "Bench Press"),
+    .init(id: 2, setsNum: 2, repsNum: 20, time: 18, title: "Squats"),
+    .init(id: 3, setsNum: 3, repsNum: 10, time: 10, title: "Planks"),
+    .init(id: 4, setsNum: 10, repsNum: 8, time: 14, title: "Situps")
+    ]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        List {
+            ForEach(posts, id: \.id) { post in
+                HStack {
+                    VStack (alignment: .leading) {
+                        Text("\(post.title)")
+                        Text("Sets: \(post.setsNum)")
+                        Text("Reps: \(post.repsNum)")
+                        HStack {
+                            Image("time")
+                            Text("\(post.time) min")
+                        }
+                    }.padding(.leading, 8)
+                    
+                    Image("time")
+                }
+                
+                
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentViews()
     }
 }
